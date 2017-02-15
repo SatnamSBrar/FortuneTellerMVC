c3d58d2a6828f22c49be00c04aa1eb0d4266e54b
Satnam Brar <satnamsbrar@gmail.com>
2017-02-15 13:56:50 -0500

    added conditions for fortune in CustomersController

--- Fortune-Teller-MVC/Controllers/CustomersController.cs
+++ Fortune-Teller-MVC/Controllers/CustomersController.cs
@@ -32,6 +32,80 @@ namespace Fortune_Teller_MVC.Controllers
             {
                 return HttpNotFound();
             }
+
+            //RETURN FORTUNE
+            //Retirement Age
+            if (customer.Age % 2 == 0)
+            {
+                ViewBag.RetirementAge = "75";
+            }
+            else
+            {
+                ViewBag.RetirementAge = "65";
+            }
+
+            //Retirement Money
+            if (customer.BirthMonth >= 1 && customer.BirthMonth <= 4)
+            {
+                ViewBag.RetirementMoney = "$500,000.00";
+            }
+            else if (customer.BirthMonth > 4 && customer.BirthMonth <= 8)
+            {
+                ViewBag.RetirementMoney = "$900,000.00";
+            }
+            else if (customer.BirthMonth > 8 && customer.BirthMonth <= 12)
+            {
+                ViewBag.RetirementMoney = "$50,000.00";
+            }
+
+            //Mode of Transportation
+            switch (customer.FavoriteColor)
+            {
+                case "Red":
+                    ViewBag.ModeOfTransportation = "Lambo";
+                    break;
+                case "Orange":
+                    ViewBag.ModeOfTransportation = "Toyota Camry";
+                    break;
+                case "Yellow":
+                    ViewBag.ModeOfTransportation = "Bentley";
+                    break;
+                case "Green":
+                    ViewBag.ModeOfTransportation = "Subaru";
+                    break;
+                case "Blue":
+                    ViewBag.ModeOfTransportation = "Ducati";
+                    break;
+                case "Indigo":
+                    ViewBag.ModeOfTransportation = "Tesla";
+                    break;
+                case "Violet":
+                    ViewBag.ModeOfTransportation = "Volkswagen";
+                    break;
+            }
+
+            //Vacation Home
+            if (customer.NumberOfSiblings == 0)
+            {
+                ViewBag.VacationHome = "Paris";
+            }
+            else if (customer.NumberOfSiblings == 1)
+            {
+                ViewBag.VacationHome = "Barcelona";
+            }
+            else if (customer.NumberOfSiblings == 2)
+            {
+                ViewBag.VacationHome = "Venice";
+            }
+            else if (customer.NumberOfSiblings == 3)
+            {
+                ViewBag.VacationHome = "Madrid";
+            }
+            else if (customer.NumberOfSiblings > 3)
+            {
+                ViewBag.VacationHome = "Miami";
+            }
+
             return View(customer);
         }
 
